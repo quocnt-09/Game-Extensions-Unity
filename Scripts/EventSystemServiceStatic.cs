b796e91ac0c558144d1896853e65a985b892c60e
quocnt <quoc.nguyen@topebox.com>
2019-09-07 00:43:20 +0700

    add first

---
+++ Scripts/EventSystemServiceStatic.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventBatchStatic
+{
+    public object target;
+    public Delegate _delegate;
+
+    public EventBatchStatic(Delegate del, object target)
+    {
+        this.target = target;
+        this._delegate = del;
+    }
+
+    public override bool Equals(object obj)
+    {
+        var p = obj as EventBatchStatic;
+        if (p == null)
+        {
+            return false;
+        }
+
+        return target == p.target && _delegate == p._delegate;
+    }
+}
+
+public static class EventSystemServiceStatic
+{
+    //FOR DEBUG
+    public static string LogTag = "EventSystemService";
+
+    public static void DispatchEnum(object obj, Enum eventName, params object[] args)
+    {
+        Dispatch(obj, eventName.GetEventString(), args);
+    }
+
+    public static void AddListenerEnum(object obj, Enum eventName, Delegate action)
+    {
+        AddListener(obj, eventName.GetEventString(), action);
+    }
+
+    public static void DispatchAllEnum(Enum eventName, params object[] args)
+    {
+        DispatchAll(eventName.GetEventString(), args);
+    }
+
+    public static void RemoveListenerEnum(object obj, Enum eventName, Delegate action)
+    {
+        RemoveListener(obj, eventName.GetEventString(), action);
+    }
+
+    public static string GetEventString(this Enum enu)
+    {
+        return "Enum_" + enu;
+    }
+
+    private static Dictionary<string, List<EventBatchStatic>> eventBatchStaticDict = new Dictionary<string, List<EventBatchStatic>>();
+
+    public static void AddListener(object obj, string eventName, Delegate action)
+    {
+        //if (obj == null) return;
+        //EventDispatcher eventDispatcher = obj.AddMissingComponent<EventDispatcher>();
+        //eventDispatcher.Add(eventName, action);
+        EventBatchStatic batch = new EventBatchStatic(action, obj);
+        if (!eventBatchStaticDict.TryGetValue(eventName, out var batches))
+        {
+            batches = new List<EventBatchStatic>();
+            eventBatchStaticDict.Add(eventName, batches);
+            batches.Add(batch);
+        }
+        else
+        {
+            if (batches.Contains(batch))
+            {
+                Debug.LogError($"Duplicate Add found event name {eventName}");
+                return;
+            }
+
+            batches.Add(batch);
+        }
+    }
+
+    private static object cTarget;
+    private static Delegate cDelegate;
+
+    public static void RemoveListener(object obj, string eventName, Delegate action)
+    {
+        //if (obj == null) return;
+        //EventDispatcher eventDispatcher = obj.AddMissingComponent<EventDispatcher>();
+        //eventDispatcher.Remove(eventName, action);
+        cTarget = obj;
+        cDelegate = action;
+        List<EventBatchStatic> batches;
+        if (!eventBatchStaticDict.TryGetValue(eventName, out batches))
+        {
+            return;
+        }
+
+        var bathToDEL = batches.Find(isMatchToRemove);
+        batches.Remove(bathToDEL);
+    }
+
+    public static void DispatchAll(string eventName, params object[] args)
+    {
+        List<EventBatchStatic> batchesOut;
+
+        if (eventBatchStaticDict.TryGetValue(eventName, out batchesOut))
+        {
+            for (int i = batchesOut.Count - 1; i >= 0; i--)
+            {
+                if (batchesOut[i]._delegate != null)
+                {
+                    //batchesOut[i]._delegate.DynamicInvoke(args);
+                    batchesOut[i]._delegate.Method.Invoke(batchesOut[i]._delegate.Target, args);
+                }
+            }
+        }
+    }
+
+    public static void Dispatch(object obj, string eventName, params object[] args)
+    {
+        //if (obj == null) return;
+        //EventDispatcher eventDispatcher = obj.AddMissingComponent<EventDispatcher>();
+        //eventDispatcher.Trigger(eventName, args);
+        List<EventBatchStatic> batchesOut;
+
+        if (eventBatchStaticDict.TryGetValue(eventName, out batchesOut))
+        {
+            for (int i = batchesOut.Count - 1; i >= 0; i--)
+            {
+                var count = batchesOut.Count;
+                if (i < count && batchesOut[i]._delegate != null)
+                {
+                    if (batchesOut[i].target == null || batchesOut[i].target == obj)
+                    {
+                        //batchesOut[i]._delegate.DynamicInvoke(args);
+                        batchesOut[i]._delegate.Method.Invoke(batchesOut[i]._delegate.Target, args);
+                    }
+                }
+            }
+        }
+    }
+
+    private static bool isMatchToRemove(EventBatchStatic b)
+    {
+        return cTarget == b.target && cDelegate == b._delegate;
+    }
+
+    private static bool isMatchToRemoveAll(EventBatchStatic b)
+    {
+        return true;
+    }
+
+    public static string GetName()
+    {
+        return "Event System Service";
+    }
+
+    public static string GetId()
+    {
+        return "namnh.service.eventsystem";
+    }
+
+    public static void RemoveAll(string[] listKeys)
+    {
+        for (int i = 0; i < listKeys.Length; i++)
+        {
+            var eventName = listKeys[i];
+            List<EventBatchStatic> batches;
+            if (!eventBatchStaticDict.TryGetValue(eventName, out batches))
+            {
+                continue;
+            }
+
+            batches.Clear();
+            //batches.RemoveWhere(isMatchToRemoveAll);
+        }
+    }
+}
---
+++ Scripts/GameExtensions.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public static class GameExtensions
+{
+    public static readonly string TIME_FORMAT = "yyyy-MM-dd HH:mm:ss tt";
+
+    #region Encrypt && Decrypt
+
+    public static string Encrypt(string input, string password)
+    {
+        byte[] data = UTF8Encoding.UTF8.GetBytes(input);
+        using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+        {
+            byte[] key = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(password));
+            using (TripleDESCryptoServiceProvider trip = new TripleDESCryptoServiceProvider() {Key = key, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7})
+            {
+                ICryptoTransform tr = trip.CreateEncryptor();
+                byte[] result = tr.TransformFinalBlock(data, 0, data.Length);
+                return Convert.ToBase64String(result, 0, result.Length);
+            }
+        }
+    }
+
+    public static string Decrypt(string input, string password)
+    {
+        byte[] data = Convert.FromBase64String(input);
+        using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+        {
+            byte[] key = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(password));
+            using (TripleDESCryptoServiceProvider trip = new TripleDESCryptoServiceProvider() {Key = key, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7})
+            {
+                ICryptoTransform tr = trip.CreateDecryptor();
+                byte[] result = tr.TransformFinalBlock(data, 0, data.Length);
+                return UTF8Encoding.UTF8.GetString(result);
+            }
+        }
+    }
+
+    #endregion
+
+    private static string _strName;
+
+    public static string TrimName(string name, string extent, int numCharacter = 8)
+    {
+        if (name.Length > numCharacter)
+        {
+            _strName = $"{name.Substring(0, numCharacter)}{extent}";
+        }
+        else
+        {
+            _strName = name;
+        }
+
+        return _strName;
+    }
+
+    #region ENUM
+
+    public static T IntToEnum<T>(int value)
+    {
+        return (T) Enum.ToObject(typeof(T), value);
+    }
+
+    public static T ParseEnum<T>(string value)
+    {
+        return (T) Enum.Parse(typeof(T), value, true);
+    }
+
+    #endregion
+
+    #region INT
+
+    public static int EnumToIn(Enum value)
+    {
+        return Convert.ToInt32(value);
+    }
+
+    public static int BoolToIn(bool value)
+    {
+        return value ? 1 : 0;
+    }
+
+    public static int StringToInt(string str)
+    {
+        str = str.Replace(",", "");
+
+        int rs = 0;
+        try
+        {
+            rs = int.Parse(str);
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("Parse To Int Error: " + e);
+        }
+
+        return rs;
+    }
+
+    #endregion
+
+    #region FLOAT
+
+    public static float StringToFloat(string str)
+    {
+        str = str.Replace(",", "");
+
+        float rs = 0;
+        try
+        {
+            rs = float.Parse(str);
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("Parse To Float Error: " + e);
+        }
+
+        return rs;
+    }
+
+    #endregion
+
+    #region BOOL
+
+    public static bool IntToBool(int value)
+    {
+        return value == 1 ? true : false;
+    }
+
+    public static bool StringToBool(string str)
+    {
+        bool rs = false;
+        try
+        {
+            rs = bool.Parse(str);
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("Parse To Bool Error: " + e);
+        }
+
+        return rs;
+    }
+
+    #endregion
+
+    #region FOR UI && GAMEOBJECT
+
+    public static bool LayerInLayerMask(int layer, LayerMask layerMask)
+    {
+        return ((1 << layer) & layerMask) != 0;
+    }
+
+    public static LayerMask AddLayerMask(this LayerMask mask, string layer)
+    {
+        mask = mask | (1 << LayerMask.NameToLayer(layer));
+        return mask;
+    }
+
+    public static LayerMask RemoveLayerMask(this LayerMask mask, string layer)
+    {
+        mask = mask & ~(1 << LayerMask.NameToLayer(layer));
+        return mask;
+    }
+
+    public static void SetSafeEnable(this SpriteRenderer sp, bool value)
+    {
+        if (sp.enabled != value)
+        {
+            sp.enabled = value;
+        }
+    }
+
+    public static void SetSafeActive(this GameObject obj, bool value)
+    {
+        if (obj == null) return;
+
+        if (obj.activeSelf != value)
+        {
+            obj.SetActive(value);
+        }
+    }
+
+    public static bool IsPointerOverUIObject(Vector2 screenPosition)
+    {
+        PointerEventData pointerData = new PointerEventData(EventSystem.current) {position = Input.mousePosition};
+        var results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(pointerData, results);
+
+        if (results.Count > 0)
+        {
+            for (int i = 0; i < results.Count; i++)
+            {
+                if (results[i].gameObject.GetComponent<Image>())
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static void SetTimeText(this TextMeshProUGUI obj, int totalSeconds)
+    {
+        var time = new TimeSpan(0, 0, totalSeconds);
+
+        var strTime = "";
+        if ((int) time.TotalHours > 0)
+        {
+            strTime = $"{time.TotalHours:00}:{time.Minutes:00}:{time.Seconds:00}";
+        }
+        else
+        {
+            if (time.Minutes > 0)
+            {
+                strTime = $"{time.Minutes:00}:{time.Seconds:00}";
+            }
+            else
+            {
+                strTime = $"{time.Seconds:00}";
+            }
+        }
+
+        obj.SetText(strTime);
+    }
+
+    #endregion
+}
